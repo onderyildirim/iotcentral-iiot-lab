469a046a76c90cead0687d89f4926a66da71737f
Onder yildirim <ondery@microsoft.com>
2022-06-02 12:31:20 -0400

    1

---
+++ resetvars.sh
@@ -0,0 +1,24 @@
+cd ~/iotcentral-iiot-lab 
+rgname="iotcoeehol"
+regionname="eastus2"
+instanceid="$RANDOM"
+instanceid="444444"
+appname="iotcoeehol-$instanceid"
+appid=$(az iot central app show -n $appname  --query applicationId --output tsv)
+networkName="iiothol-network-$instanceid"
+opcVM="opcdevicevm-$instanceid"
+plcVM="plcdevicevm-$instanceid"
+adminUserSshPublicKey=$(cat $(readlink -f ~/.ssh/id_rsa.pub))
+
+echo "================= Variables set ==============="
+echo "   rgname=$rgname"
+echo "   regionname=$regionname"
+echo "   instanceid=$instanceid"
+echo "   appname=$appname"
+echo "   networkName=$networkName"
+echo "   opcVM=$opcVM"
+echo "   plcVM=$plcVM"
+echo "   adminUserSshPublicKey=$adminUserSshPublicKey"
+echo "=============================================="
+
+
